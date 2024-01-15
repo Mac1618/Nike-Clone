b563543f9a3ae9b0085517061602d141ef92a76f
unknown <you@example.com>
2024-01-15 22:37:48 +0800

    style: Added the Super Quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ const Button = ({ label, iconURL }) => {
 	return (
 		<button className="bg-coral-red flex px-7 py-4 rounded-full text-white font-montserrat border gap-2">
 			{label}
-			<img className="ml-2 rounded-full w-5 h-6" src={iconURL} alt="arrow-right-icon" />
+			{iconURL && (
+				<img className="ml-2 rounded-full w-5 h-6" src={iconURL} alt="arrow-right-icon" />
+			)}
 		</button>
 	);
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,41 @@
-const SuperQuality = () => {
-  return (
-    <div>SuperQuality</div>
-  )
-}
+// components
+import Button from '../components/Button';
 
-export default SuperQuality
+// images and icons
+import { shoe8 } from '../assets/images';
+
+const SuperQuality = () => {
+	return (
+		<section
+			id="about-us"
+			className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+		>
+			<div className="flex flex-1 flex-col">
+				<h1 className="font-bold text-4xl font-palanquin capitalize max-sm:leading-[82px] max-sm:text-[72px] lg:max-w-lg">
+					We Provide You
+					<span className="text-coral-red"> Super </span>
+					<br />
+					<span className="text-coral-red"> Quality </span>
+					Shoes
+				</h1>
+				<p className="mt-4 info-text lg:max-w-lg">
+					Ensuring premium comfort and style, our meticulously crafted footwear is designed to
+					elevate your experience, providing you with unmatched quality, innovation, and a touch of
+					elegance.
+				</p>
+				<p className="mt-6 info-text lg:max-w-lg">
+					Our dedication to detail and excellence ensures your satisfaction
+				</p>
+				<div className="mt-11">
+					<Button label="View Details" />
+				</div>
+			</div>
+
+			<div className="flex flex-1 justify-center items-center">
+				<img src={shoe8} alt="" width={570} height={522} className="" />
+			</div>
+		</section>
+	);
+};
+
+export default SuperQuality;
