dafd21e6c6d2fc8ab0cc727a1ebcff85bd41ff3c
unknown <you@example.com>
2024-01-16 01:16:26 +0800

    style: Added the Special Offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
 	return (
-		<button className="bg-coral-red flex px-7 py-4 rounded-full text-white font-montserrat border gap-2">
+		<button
+			className={` flex px-7 py-4 rounded-full font-montserrat border gap-2
+				${
+					backgroundColor
+						? `${backgroundColor} ${borderColor} ${textColor}`
+						: 'text-white bg-coral-red border-coral-red'
+				}`}
+		>
 			{label}
 			{iconURL && (
 				<img className="ml-2 rounded-full w-5 h-6" src={iconURL} alt="arrow-right-icon" />
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,37 @@
-const SpecialOffer = () => {
-  return (
-    <div>SpecialOffer</div>
-  )
-}
+// icons and images
+import { offer } from '../assets/images'
+import { arrowRight } from '../assets/icons';
 
-export default SpecialOffer
+// components
+import Button from '../components/Button';
+
+const SpecialOffer = () => {
+	return (
+		<section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+			<div className="flex-1">
+				<img src={offer} alt="Offer image" width={773} height={687} />
+			</div>
+
+			<div className="flex flex-1 flex-col">
+				<h1 className="font-bold text-4xl font-palanquin capitalize max-sm:leading-[82px] max-sm:text-[72px]">
+					<span className="text-coral-red">Special </span>
+					Offer
+				</h1>
+				<p className="mt-4 info-text ">
+					Embark on a shopping journey that redefines your experience with unbeatable deals. From
+					premier selections to incredible savings, we offer unparalleled value that sets us apart.
+				</p>
+				<p className="mt-6 info-text">
+					Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the
+					loftiest expectations. Your journey with us is nothing short of exceptional.
+				</p>
+				<div className="flex flex-wrap mt-11 gap-6">
+					<Button label="Shop now" iconURL={arrowRight}/>
+          <Button label="Learn more" backgroundColor='bg-white' borderColor='border-slate-gray'  textColor='text-slate-gray' />
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SpecialOffer;
